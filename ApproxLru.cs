bc98c7ada57dfabadc2b500ea8c111c80ee8eb69
Krzysztof Ruczkowski <tojatos@gmail.com>
2019-04-08 23:56:53 +0200

    Approx LRU done

---
+++ ApproxLru.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PageReplacementAlgorithms
+{
+    public class ApproxLru
+    {
+        public int Simulate(List<int> items)
+        {
+            var requestsToTake = new Queue<int>(items);
+            var lruList = new List<int>();
+            var secondChanceSet = new HashSet<int>();
+            var frameBuffer = new List<int>();
+            int pageFaults = 0;
+            while (requestsToTake.Any())
+            {
+                int request = requestsToTake.Dequeue();
+
+                if (lruList.Contains(request)) lruList.Remove(request);
+                lruList.Add(request);
+
+                if (frameBuffer.Contains(request))
+                {
+                    secondChanceSet.Add(request);
+                    continue;
+                }
+                if (frameBuffer.Count == Program.FrameCount)
+                {
+                    int? leastRecentlyUsed = null;
+                    foreach (int i in lruList.FindAll(i => frameBuffer.Contains(i)))
+                    {
+                        if (secondChanceSet.Contains(i))
+                        {
+                            secondChanceSet.Remove(i);
+                            continue;
+                        }
+
+                        leastRecentlyUsed = i;
+                        break;
+                    }
+
+                    frameBuffer.Remove(leastRecentlyUsed ?? lruList.First(i => frameBuffer.Contains(i)));
+                }
+                frameBuffer.Add(request);
+                ++pageFaults;
+                //Console.WriteLine(string.Join(", ", frameBuffer).TrimEnd(',', ' '));
+                //Console.WriteLine(string.Join(", ", secondChanceSet).TrimEnd(',', ' '));
+            }
+
+            return pageFaults;
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ namespace PageReplacementAlgorithms
 {
     internal static class Program
     {
-        private const int TestSeries = 30;
-        public const int PageCount = 100;
+        private const int TestSeries = 50;
+        public const int PageCount = 20;
         public const int FrameCount = 10;
         public const int RequestCount = 2000;
         private static readonly Random Random = new Random();
@@ -16,12 +16,14 @@ namespace PageReplacementAlgorithms
             var fcfsFaults = new List<int>();
             var optFaults = new List<int>();
             var lruFaults = new List<int>();
+            var approxLruFaults = new List<int>();
             var randFaults = new List<int>();
             try
             {
                 var fcfs = new Fifo();
                 var opt = new Opt();
                 var lru = new Lru();
+                var approxLru = new ApproxLru();
                 var rand = new Rand();
                 for (int i = 0; i < TestSeries; ++i)
                 {
@@ -29,11 +31,14 @@ namespace PageReplacementAlgorithms
                     fcfsFaults.Add(fcfs.Simulate(requests));
                     optFaults.Add(opt.Simulate(requests));
                     lruFaults.Add(lru.Simulate(requests));
+                    approxLruFaults.Add(approxLru.Simulate(requests));
+                    //approxLruFaults.Add(approxLru.Simulate(new List<int>{0, 4, 1, 4, 2, 4, 3, 4, 2, 4, 0, 4, 1, 4, 2, 4, 3, 4}));
                     randFaults.Add(rand.Simulate(requests));
                 }
                 Console.WriteLine($"FIFO average faults: {fcfsFaults.Average()}");
                 Console.WriteLine($"OPT average faults: {optFaults.Average()}");
                 Console.WriteLine($"LRU average faults: {lruFaults.Average()}");
+                Console.WriteLine($"Approx LRU average faults: {approxLruFaults.Average()}");
                 Console.WriteLine($"RAND average faults: {randFaults.Average()}");
             }
             catch (Exception e)
